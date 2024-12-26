6ef38d05a420be1a00f3a8b292bebad9f14ba3a6
ArtemEvgTitov <artem.titov.wg@gmail.com>
2024-12-26 16:05:26 +0300

    refactor: createNDimensionalArray done

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -291,7 +291,7 @@ function distinct(arr) {
     return arr;
   }
   const result = [];
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i += 1) {
     if (!result.includes(arr[i])) {
       result.push(arr[i]);
     }
@@ -312,8 +312,12 @@ function distinct(arr) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  if (n === 1) {
+    return new Array(size).fill(0);
+  }
+  const innerArray = createNDimensionalArray(n - 1, size);
+  return new Array(size).fill(innerArray);
 }
 
 /**
